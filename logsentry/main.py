f83d9c4144e13812730fe70f11b9a6fb688cbdf2
lagunamanuel <lagunamanuel8@gmail.com>
2026-07-03 17:39:34 +0200

    feat(api): add 15s delay between requests to respect rate limit

--- logsentry/main.py
+++ logsentry/main.py
@@ -1,4 +1,6 @@
 import argparse
+import time
+
 from logsentry.parser import extract_ips_from_log
 from logsentry.api import check_ip_virustotal
 
@@ -9,18 +11,19 @@ def main():
     Parses arguments, extracts IPs, and queries the VirusTotal API.
     """
     parser = argparse.ArgumentParser(description="LogSentry: CLI tool to analyze log IPs.")
-    parser.add_argument("-l","--log", required=True, help="Path to the log file to analyze")
-    parser.add_argument("-t","--threshold", type = int,default=5, help="Minimum number of failed attempts to consider an IP suspicious")
+    parser.add_argument("-l", "--log", required=True, help="Path to the log file to analyze")
+    parser.add_argument("-t", "--threshold", type=int, default=5,
+                        help="Minimum number of failed attempts to consider an IP suspicious")
 
     args = parser.parse_args()
 
     print(f"[*] Analyzing log file: {args.log}")
-    ips = extract_ips_from_log(args.log,args.threshold)
-
+    ips = extract_ips_from_log(args.log, args.threshold)
 
     print(f"[*] Found {len(ips)} unique IPs.")
 
-    for ip in ips:
+    total = len(ips)
+    for index, ip in enumerate(ips):
         print(f"\n[*] Checking IP: {ip}...")
         vt_data = check_ip_virustotal(ip)
 
@@ -34,6 +37,9 @@ def main():
                 print(f"[-] IP {ip} -> VirusTotal: clean")
         else:
             print(f"[-] Could not retrieve data for {ip}")
+        if index < total - 1:
+            print(f"[!] Waiting 15 seconds to respect rate limit...")
+            time.sleep(15)
 
 
 if __name__ == "__main__":
