66c2df679af7d8e159eeb0c1220dbce404ad90b3
lagunamanuel <lagunamanuel8@gmail.com>
2026-07-16 19:06:58 +0200

    docs(parser): add standard docstrings to extract_ips_from_log

--- logsentry/parser.py
+++ logsentry/parser.py
@@ -7,10 +7,17 @@ def extract_ips_from_log(log_path: str, threshold: int = 5) -> list:
     """
     Reads a text log file and extracts valid IPv4 addresses
     that exceed the given failure threshold.
+
+    Args:
+        log_path (str): Path to the log file
+        threshold (int): Minimum number of occurrences required to extract an IP.
+
+    Returns:
+        list: A list of string IPv4 addresses that met or exceeded the threshold.
     """
 
 
-    ip_counts = {}
+
     ipv4_pattern = r'\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b'
     try:
         with open(log_path, 'r') as file:
