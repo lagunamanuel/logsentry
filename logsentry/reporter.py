8f7960cfb48c52823499912f97ab83cf1beeb2ef
lagunamanuel <lagunamanuel8@gmail.com>
2026-07-31 20:17:46 +0200

    docs(reporter): add docstring to export_to_csv

--- logsentry/reporter.py
+++ logsentry/reporter.py
@@ -2,6 +2,13 @@ import csv
 
 
 def export_to_csv(data: list,filename: str)->None:
+    """
+        Exports a list of dictionaries containing IP data to a CSV file.
+
+        Args:
+            data (list): List of dictionaries with 'ip' and 'malicious_engines' keys.
+            filename (str): The output CSV filename.
+        """
 
     with open(filename, mode="w", newline="") as archivo:
 
